b164d453c6b9c38eb6ce8289b7d0fd50f7f7f15f
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-16 12:55:52 +0600

    design completed for home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'
 import Header from '../Header/Header';
+import tripData from '../../fakeData/trips'
+import trip1 from '../../images/Image/Rectangle 1.png';
+import trip2 from '../../images/Image/Sreemongol.png';
+import trip3 from '../../images/Image/sundorbon.png';
+
+
+import { Button } from '@material-ui/core';
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 
 const Home = () => {
+    const [trips, setTrips] = useState(tripData);
+    const trip = trips.find(tp => tp.id === 1)
+    console.log(trip);
     return (
         <div  className="home">
-            
             <Header/>
+            <div className="trip__option">
+                <div className="trip__info">
+                    <h1> {trip.name} </h1>
+                    <p> {trip.description} </p>
+                    <div className="button"> <p>booking</p> <ArrowRightAltIcon/> </div>
+                </div>
+                <div className="trip__img">
+                    <img src={trip1} alt=""/>
+                    <img src={trip2} alt=""/>
+                    <img src={trip3} alt=""/>
+                </div>
+            </div>
+        
         </div>
     );
 };
---
+++ src/fakeData/trips.js
@@ -0,0 +1,25 @@
+import trip1 from '../images/Image/Rectangle 1.png';
+import trip2 from '../images/Image/Sreemongol.png';
+import trip3 from '../images/Image/sundorbon.png';
+
+const trips = [
+    {
+        id: 1,
+        name: "cox's bazar",
+        description: "Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...",
+        img: trip1,
+    },
+    {
+        id: 2,
+        name: "Sreemangal",
+        description: "Sreemangal fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...",
+        img: trip2,
+    },
+    {
+        id: 3,
+        name: "Sundarbans",
+        description: "Sundarbans fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...",
+        img: trip3,
+    },
+];
+export default trips;
