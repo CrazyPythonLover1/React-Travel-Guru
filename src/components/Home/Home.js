f0d56dafc72f678aae9732910e783df7c5396c86
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-16 16:21:09 +0600

    ActiveStyle on the trip

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,6 @@ import tripData from '../../fakeData/trips'
 import trip1 from '../../images/Image/Rectangle 1.png';
 import trip2 from '../../images/Image/Sreemongol.png';
 import trip3 from '../../images/Image/sundorbon.png';
-
-
 import { Button } from '@material-ui/core';
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 
@@ -16,34 +14,38 @@ const Home = () => {
     const trip = trips.find(tp => tp.id === id)
 
     const handleTrip = (id) => {
-        console.log(id,"clicked");
         setId(id);
     }
-
+    
+    const activeStyle = {
+        border: '3px solid orange',
+    }
     return (
         <div  className="home">
             <Header/>
+
             <div className="trip__option">
                 <div className="trip__info">
                     <h1> {trip.name} </h1>
                     <p> {trip.description} </p>
                     <div className="button"> <p>booking</p> <ArrowRightAltIcon/> </div>
                 </div>
+
                 <div className="trip__img">
                     <div className="img">
-                        <img onClick={()=>handleTrip(1)} src={trip1} alt=""/>
+                        <img style={id===1?activeStyle:{}} onClick={()=>handleTrip(1)} src={trip1} alt=""/>
                         <h3> COX'S BAZAR </h3>
                     </div>
                     <div className="img">
-                        <img onClick={()=>handleTrip(2)} src={trip2} alt=""/>
+                        <img style={id===2?activeStyle:{}} onClick={()=>handleTrip(2)} src={trip2} alt=""/>
                         <h3> SREEMANGAL</h3>
                     </div>
                     <div className="img">
-                        <img onClick={()=>handleTrip(3)} src={trip3} alt=""/>
+                        <img style={id===3?activeStyle:{}} onClick={()=>handleTrip(3)} src={trip3} alt=""/>
                         <h3> SUNDARBAN </h3>
                     </div>
-                    
                 </div>
+
             </div>
         
         </div>
